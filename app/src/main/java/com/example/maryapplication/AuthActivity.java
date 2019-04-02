ecf2b81680cf1422a05835523ac4eeda52557963
svetlana <svetlana.klevetova92@yandex.ru>
2019-04-02 08:54:48 +0300

    Adding fragments

--- app/src/main/java/com/example/maryapplication/AuthActivity.java
+++ app/src/main/java/com/example/maryapplication/AuthActivity.java
@@ -1,83 +1,19 @@
 package com.example.maryapplication;
 
-import android.content.Intent;
-import android.os.UserHandle;
-import android.support.annotation.StringRes;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.util.regex.Pattern;
+import android.support.v4.app.Fragment;
 
-public class AuthActivity extends AppCompatActivity {
+/**
+ * Created by tanchuev on 08.11.2017.
+ */
 
-    private EditText mLogin;
-    private EditText mPassword;
-
-    private Button mEnter;
-    private Button mRegister;
-
-
-
-
-    private View.OnClickListener mOnEnterClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if(isEmailValid() && isPasswordValid())
-            {
-                Intent startProfileIntent = new Intent( AuthActivity.this, ProfileActivity.class);
-                startProfileIntent.putExtra(ProfileActivity.USER_KEY, new User (mLogin.getText().toString(),mPassword.getText().toString()) );
-                startActivity(startProfileIntent);
-
-            }
-            else
-            {
-                showMessage( R.string.inputError );
-            }
-        }
-    };
-
-    private View.OnClickListener mOnRegisterClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-        }
-    };
-
-    private void showMessage(@StringRes int string)
-    {
-        Toast.makeText( this, string, Toast.LENGTH_LONG ).show();
-    }
-
-    private boolean isEmailValid()
-    {
-        return !TextUtils.isEmpty( mLogin.getText())
-        && Patterns.EMAIL_ADDRESS.matcher( mLogin.getText()).matches();
-    }
-
-    private boolean isPasswordValid()
-    {
-        return !TextUtils.isEmpty( mLogin.getText());
-    }
+public class AuthActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.ac_auth );
-
-        mLogin = findViewById( R.id.etLogin );
-        mPassword = findViewById( R.id.etPasssword );
-
-        mEnter = findViewById( R.id.buttonEnter );
-        mRegister = findViewById( R.id.buttonRegister );
-
-        mEnter.setOnClickListener( mOnEnterClickListener );
-        mRegister.setOnClickListener( mOnRegisterClickListener );
-
+    protected Fragment getFragment() {
+        return AuthFragment.newInstance();
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/maryapplication/AuthFragment.java
@@ -0,0 +1,92 @@
+package com.example.maryapplication;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AuthFragment extends Fragment {
+
+    private EditText mLogin;
+    private EditText mPassword;
+
+    private Button mEnter;
+    private Button mRegister;
+
+    public static AuthFragment newInstance() {
+        Bundle args = new Bundle();
+
+        AuthFragment fragment = new AuthFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+
+    private View.OnClickListener mOnEnterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(isEmailValid() && isPasswordValid())
+            {
+                Intent startProfileIntent = new Intent(getActivity(), ProfileActivity.class);
+                startProfileIntent.putExtra(ProfileActivity.USER_KEY, new User (mLogin.getText().toString(),mPassword.getText().toString()) );
+                startActivity(startProfileIntent);
+
+            }
+            else
+            {
+                showMessage( R.string.inputError );
+            }
+        }
+    };
+
+    private View.OnClickListener mOnRegisterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    private void showMessage(@StringRes int string)
+    {
+        Toast.makeText( getActivity(), string, Toast.LENGTH_LONG ).show();
+
+    }
+
+    private boolean isEmailValid()
+    {
+        return !TextUtils.isEmpty( mLogin.getText())
+        && Patterns.EMAIL_ADDRESS.matcher( mLogin.getText()).matches();
+    }
+
+    private boolean isPasswordValid()
+    {
+        return !TextUtils.isEmpty( mLogin.getText());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fr_auth, container, false);
+
+        mLogin = v.findViewById(R.id.etLogin);
+        mPassword = v.findViewById(R.id.etPasssword);
+        mEnter = v.findViewById(R.id.buttonEnter);
+        mRegister = v.findViewById(R.id.buttonRegister);
+
+        mEnter.setOnClickListener(mOnEnterClickListener);
+        mRegister.setOnClickListener(mOnRegisterClickListener);
+
+        return v;
+    }
+}
